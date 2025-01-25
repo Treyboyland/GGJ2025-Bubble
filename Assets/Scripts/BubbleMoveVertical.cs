250518af03cbbcaa432931bc3f9d02b7c78d1c5f
Chris Fibich <pixelmonium@gmail.com>
2025-01-25 14:19:51 -0600

    Made it so bubbles subtle horizontal movement
    Makes them look more bubble like

--- Assets/Scripts/BubbleMoveVertical.cs
+++ Assets/Scripts/BubbleMoveVertical.cs
@@ -8,11 +8,32 @@ public class BubbleMoveVertical : MonoBehaviour
     [SerializeField]
     Rigidbody2D body;
 
+    float timer = 0.0f;
+    const float HORZ_FORCE = 30.0f;
+
     /// <summary>
     /// This function is called when the object becomes enabled and active.
     /// </summary>
     void OnEnable()
     {
         body.linearVelocity = new Vector2(0, bubble.Stats.Speed);
+        ResetTimer();
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        timer = Mathf.Max(timer, 0.0f);
+
+        if (timer <= 0.0f)
+        {
+            body.AddForce(new Vector2(Random.Range(-HORZ_FORCE, HORZ_FORCE), 0.0f));
+            ResetTimer();
+        }
+    }
+
+    private void ResetTimer()
+    {
+        timer = Random.Range(0.5f, 2.0f);
     }
 }
