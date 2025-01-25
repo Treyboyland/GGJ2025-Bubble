3301b51982e709f5039619be259a7b772cf99143
trgrote <grotese@gmail.com>
2025-01-25 16:19:08 -0600

    Add ScoreHandler

---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using UnityEngine;
+
+public class ScoreHandler : MonoBehaviour
+{
+    [SerializeField] rho.RuntimeInt score;
+    [SerializeField] rho.RuntimeGameObjectSet eggs;
+
+    #region Event Handlers
+    public void ModifyScore(int amount)
+    {
+        score.Value += amount;
+    }
+    #endregion
+    
+    void SetScoreToSumOfAllEggHealth()
+    {
+        score.Value = eggs.Select(e => e.GetComponent<EggHealth>())
+            .Where(eh => eh != null)
+            .Sum(eh => eh.Health);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() => SetScoreToSumOfAllEggHealth();
+}
