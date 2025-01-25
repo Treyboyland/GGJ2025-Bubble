40061b239d1b9040d4159249b53c97db9665b3cc
Roosevelt Boyland III <treyboyland@aol.com>
2025-01-25 17:16:08 -0600

    Adding score runtime int to UI

--- Assets/Scripts/UI/ScoreTextUI.cs
+++ Assets/Scripts/UI/ScoreTextUI.cs
@@ -1,3 +1,4 @@
+using rho;
 using TMPro;
 using UnityEngine;
 
@@ -6,17 +7,20 @@ public class ScoreTextUI : MonoBehaviour
     [SerializeField]
     TMP_Text textBox;
 
+    [SerializeField]
+    RuntimeInt scoreValue;
+
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
     private void Start()
     {
-        UpdateText(0);
+        UpdateText();
     }
 
-    public void UpdateText(int score)
+    public void UpdateText()
     {
-        textBox.text = "Points: " + string.Format("{0:D4}", score);
+        textBox.text = "Points: " + string.Format("{0:D4}", scoreValue.Value);
     }
 }
