9bdbc35128609478c71e90c2f2bf04196af7a452
Chris Fibich <pixelmonium@gmail.com>
2025-01-26 11:25:04 -0600

    Changed bubble spawn location
    Balanced bubble speed and egg size

--- Assets/Scripts/BubbleMoveVertical.cs
+++ Assets/Scripts/BubbleMoveVertical.cs
@@ -9,15 +9,15 @@ public class BubbleMoveVertical : MonoBehaviour
     Rigidbody2D body;
 
     float timer = 0.0f;
-    const float HORZ_FORCE = 30.0f;
-    const float RND_SPEED_RNG = 0.28f;
+    const float HORZ_FORCE = 28.0f;
+    const float RND_SPEED_RNG = 0.35f;
 
     /// <summary>
     /// This function is called when the object becomes enabled and active.
     /// </summary>
     void OnEnable()
     {
-        body.linearVelocity = new Vector2(0, bubble.Stats.Speed + Random.Range(-RND_SPEED_RNG, RND_SPEED_RNG));
+        body.linearVelocity = new Vector2(0, bubble.Stats.Speed + Random.Range(-RND_SPEED_RNG / 2.0f, RND_SPEED_RNG));
         ResetTimer();
     }
 
@@ -35,6 +35,6 @@ public class BubbleMoveVertical : MonoBehaviour
 
     private void ResetTimer()
     {
-        timer = Random.Range(0.5f, 2.0f);
+        timer = Random.Range(0.25f, 1.0f);
     }
 }
